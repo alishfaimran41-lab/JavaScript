322de711aee3bcf430435e267a2e13a1d75ae08d
Alishfa imran <alishfaimran41@gmail.com>
2025-10-04 21:49:42 -0700

    Add files via upload

---
+++ alishfa.js
@@ -0,0 +1,36 @@
+// console.log("Hello")
+// let number=5;
+// if (number % 2=== 0 ){
+//     console.log(number + "is Even");
+// }else{
+//     console.log(number + "is odd");
+// }
+// let number=7;
+// if (number > 0){
+//     console.log(number + "is positive");
+// } else if (number < 0){
+//     console.log(number + "is negative");
+// } else {
+//     console.log('the number is zero');
+// }
+// let num1=15;
+// let num2=20;
+// if (num1 > num2) {
+//     console.log(num1 +"is larger than" +num2 );
+// } else if (num2 > num1) {
+//     console.log(num2 + "is larger than" + num1);
+// } else {
+//     console.log("Both numbers are equal");
+// }
+// let age=16;
+// if (age >= 18){
+//     console.log("Eligible for vote");
+// } else {
+//     console.log("Not eligible for vote");
+// }
+// let marks=45;
+// if (marks >= 33) {
+//     console.log("pass");
+// } else {
+//     console.log("Fail");
+// }
